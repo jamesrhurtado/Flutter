69273757f7d96a8d600858da8b85b2094d9fc58a
jrenzzo <u202118568@upc.edu.pe>
2022-10-17 17:18:41 -0500

    chore(app): Added app to practice routing

---
+++ app_routing/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Route!"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        child: Text("Open other route"),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+        },
+      )),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route!"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        child: Text("Go back"),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      )),
+    );
+  }
+}

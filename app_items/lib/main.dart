61ce2c9ef73918cc68a191950dd235aec6ae3c9f
jrenzzo <u202118568@upc.edu.pe>
2022-10-20 15:26:02 -0500

    Added App Items to practice List View

---
+++ app_items/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    return MaterialApp(
+        title: "Welcome",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome!"),
+          ),
+          body: Center(
+            child: RandomWords()
+          ),
+        ));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
